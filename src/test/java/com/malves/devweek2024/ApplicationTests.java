27545ad407011f4e06e2348e33bd3100c0b18fbc
Michel Alves <malvesdesign@hotmail.com>
2024-04-20 17:17:58 -0300

    Initial commit

---
+++ src/test/java/com/malves/devweek2024/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malves.devweek2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
