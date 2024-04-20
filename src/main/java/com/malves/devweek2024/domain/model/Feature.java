053bcd376bb8d3c4e1f16be3e5ca1976d4551c27
Michel Alves <malvesdesign@hotmail.com>
2024-04-20 19:55:19 -0300

    Implementação na camada de domínio

---
+++ src/main/java/com/malves/devweek2024/domain/model/Feature.java
@@ -0,0 +1,11 @@
+package com.malves.devweek2024.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/com/malves/devweek2024/domain/model/News.java
@@ -0,0 +1,8 @@
+package com.malves.devweek2024.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
