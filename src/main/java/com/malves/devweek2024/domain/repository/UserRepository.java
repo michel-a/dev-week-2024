3a87cb0c88ce93975bfb1888e1c62105d47826a5
Michel Alves <malvesdesign@hotmail.com>
2024-04-20 21:36:02 -0300

    Adicionado um novo método ao UserRepository

--- src/main/java/com/malves/devweek2024/domain/repository/UserRepository.java
+++ src/main/java/com/malves/devweek2024/domain/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.malves.devweek2024.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
 }
